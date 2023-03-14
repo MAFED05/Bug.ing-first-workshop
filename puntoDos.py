cc931b02e722a84937d68a8f66d73f53f1cd244d
MAFED05 <124614151+MAFED05@users.noreply.github.com>
2023-03-14 17:20:50 -0500

    Add files via upload

---
+++ puntoDos.py
@@ -0,0 +1,12 @@
+a : float
+b : float
+c : float
+a =float (input("Ingrese un número real "))
+b =float (input("Ingrese un número real "))
+c =float (input("Ingrese un número real "))
+if a>b and a>c :
+    print ("El número mayor es " + (str(a)))
+if b>a and b>c : 
+    print ("El número mayor es " + (str(b)))
+if c>a and c>b :
+    print ("El número mayor es " + (str(c)))
