971cced8efd457a691684247da8724135d88732f
MAFED05 <124614151+MAFED05@users.noreply.github.com>
2023-03-14 13:51:02 -0500

    Add files via upload

---
+++ punto 2.py
@@ -0,0 +1,12 @@
+a : float
+b : float
+c : float
+a =float (input("Ingrese un número real"))
+b =float (input("Ingrese un número real"))
+c =float (input("Ingrese un número real"))
+if a>b and a>c :
+    print (str(a))
+if b>a and b>c : 
+    print (str(b))
+if c>a and c>b :
+    print (str(c))
